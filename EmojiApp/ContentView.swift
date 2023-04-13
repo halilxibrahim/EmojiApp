152eb7373b3edc4b414502c3fd130665b3a2c588
halilxibrahim <ibrahim_kamaci_06@hotmail.com>
2023-04-13 16:54:10 +0300

    first commit

--- EmojiApp/ContentView.swift
+++ EmojiApp/ContentView.swift
@@ -7,15 +7,29 @@
 
 import SwiftUI
 
+enum Emoji: String, CaseIterable{
+    case 😅, 🛰️, 👻, 👽
+}
+
 struct ContentView: View {
+    @State var selection: Emoji = .🛰️
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            VStack{
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                Picker("Emoji Seç", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self) {emoji in
+                        Text(emoji.rawValue)
+                        
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Emoji Seçici")
+            .padding()
         }
-        .padding()
     }
 }
 
