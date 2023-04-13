65fac751478741296eafd9ac972ac2ea9ffe2cf1
halilxibrahim <ibrahim_kamaci_06@hotmail.com>
2023-04-13 14:31:53 +0300

    Initial Commit

---
+++ EmojiApp/EmojiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiAppApp.swift
+//  EmojiApp
+//
+//  Created by Halil ibrahim Kamacı on 13.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct EmojiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
